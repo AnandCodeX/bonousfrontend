a3a590fe4e20590cf970963e928ce930394c6a1d
AnandCodeX <Anandrtiwari@live.com>
2023-11-22 10:58:29 +0000

    test removed

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-module.exports = {
-	collectCoverage: true,
-	collectCoverageFrom: ["src/**/*.{js,jsx}"],
-	coverageDirectory: "coverage",
-	testEnvironment: "jsdom",
-	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-};
